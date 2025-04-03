1b3e7c06f34602acce0a1bb7419967fa79022de0
Raxan7 <manyerere201@gmail.com>
2025-04-03 07:17:39 +0300

    Uploaded category name

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -10,13 +10,14 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     supplier_info = serializers.SerializerMethodField()
+    category_name = serializers.CharField(source='category.name', read_only=True)
     
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'image', 'supplier_info', 'stock', 'category']
+        fields = ['id', 'name', 'description', 'price', 'image', 'supplier_info', 'stock', 'category', 'category_name']
     
     def get_supplier_info(self, obj):
-        if obj.supplier:
+        if (obj.supplier):
             return {
                 'name': obj.supplier.name,
                 'whatsapp_link': obj.supplier.whatsapp_link
