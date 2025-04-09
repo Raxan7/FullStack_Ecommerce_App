0042f72ec9d7087ecee113947c66eb82e3702523
Raxan7 <manyerere201@gmail.com>
2025-04-09 21:26:46 +0300

    Handled CORS

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -213,6 +213,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://fullstack-ecommerce-app-kcn1.onrender.com",
     "http://localhost:3000",
     "http://localhost:8000",
+    "luxefreemarket.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
