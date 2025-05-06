6b195f2c7a56dc370bf1c6779b626979b73622b0
Raxan7 <manyerere201@gmail.com>
2025-05-06 11:05:37 +0300

    Fixed the 0 issue

--- backend/product/migrations/0016_product_user.py
+++ backend/product/migrations/0016_product_user.py
@@ -16,7 +16,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='product',
             name='user',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, related_name='products', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(
+                null=True,
+                blank=True, 
+                on_delete=django.db.models.deletion.CASCADE, 
+                related_name='products', 
+                to=settings.AUTH_USER_MODEL
+            ),
             preserve_default=False,
         ),
     ]
